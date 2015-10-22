4d54337f7d9c64f8a334baa7371c4f4302090330
adamhazem <adamhazem@hotmail.co.uk>
2015-10-22 14:05:34 +0100

    ex2
    Completion of week 5 exercise 2.

---
+++ week5.2.py
@@ -0,0 +1,11 @@
+def main():
+    a=5
+    b=7
+    print(mystery(a,b))
+def mystery(x,y):
+    z=x+y
+    z=z / 2.0
+    return z
+main()
+#This programme has printed 6.0 on the shell.
+#Has defined the varibles of a and b aswell as the formular of mystery. 
