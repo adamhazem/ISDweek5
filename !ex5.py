ce7d07a041e6b4213fe788c14114beeb71b0e4f7
adamhazem <adamhazem@hotmail.co.uk>
2015-10-22 14:55:25 +0100

    !ex5
    Incomplete exercise five.

---
+++ !ex5.py
@@ -0,0 +1,8 @@
+#Counts the number of spaces.
+spaces = 0
+for char in userInput:
+    if char == " ":
+        spaces = spaces +1
+
+def main():
+    count_spaces = int(intput("Write a sentance please")
