01d8a33cb7aa52378f599e04c1f611452dd27c2c
adamhazem <adamhazem@hotmail.co.uk>
2015-10-22 13:47:47 +0100

    Week5.1
    Answer for lab sheet week5.1

---
+++ week5.1.py
@@ -0,0 +1,8 @@
+def mystery(x,y):
+    result = (x+y)/(y-x)
+    return result
+
+print(mystery(2,3))
+
+#Originally I got the answer but went the long way through it by giving a varible at the start.
+#All I needed to do was command the definition.
